9f8db9eaf88bfcfcebe87dce31047743aa4d3148
Tsimofei <zhalianhouski@gmail.com>
2023-05-26 11:23:18 +0300

    Commit hw-19

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -11,6 +11,9 @@ export class TestingController {
     }
     await this.dataSource.query(
       `
+      DELETE FROM "comment_likes" CASCADE;
+      DELETE FROM "comments" CASCADE;
+      DELETE FROM "post_likes" CASCADE;
       DELETE FROM "devices" CASCADE;
       DELETE FROM "posts" CASCADE;
       DELETE FROM "ban_user_for_blog" CASCADE;
